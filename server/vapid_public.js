47c0d96565e39ff1594214824709ea3a45c72d71
giammarcoPiccoliExt <giammarco.piccoli.ext@leonardocompany.com>
2026-02-22 18:39:15 +0100

    yt

--- server/vapid_public.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const vapidPath = path.join(__dirname, '..', 'certs', 'vapid.json');
-module.exports = (req, res) => {
-  if (!fs.existsSync(vapidPath)) return res.status(404).send('');
-  const vapid = JSON.parse(fs.readFileSync(vapidPath));
-  res.type('text/plain').send(vapid.publicKey);
-};
